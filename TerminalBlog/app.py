f69dd635ba2672a45fd1932257289f3a5824ec51
Michael Nash <michael@spatialnetworks.com>
2019-03-26 17:26:40 -0400

    insert database class

--- TerminalBlog/app.py
+++ TerminalBlog/app.py
@@ -1,10 +1,7 @@
-__author__ = 'utumno86'
-
 from models.post import Post
 import pymongo
+__author__ = 'utumno86'
 
-uri = "mongodb://127.0.0.1:27017"
-client = pymongo.MongoClient(uri)
 
 post = Post("Post 1 title", "post 1 content", "post 1 author")
 post2 = Post("Post 2 title", "post 2 content", "post 2 author")
---
+++ TerminalBlog/database.py
@@ -0,0 +1,24 @@
+import pymongo
+
+__author__ = 'utumno86'
+
+class Database(object):
+  URI = "mongodb://127.0.0.1:27017"
+  DATABASE = None
+
+  @staticmethod
+  def initialize():
+    client = pymongo.MongoClient(Database.URI)
+    Database.DATABASE = client['fullstack']
+
+  @staticmethod
+  def insert(collection, data):
+    Database.DATABASE[collection].insert(data)
+
+  @staticmethod
+  def find(collection, query):
+    Database.DATABASE[collection].find(query)
+
+  @staticmethod
+  def find_one(collection, query):
+    Database.DATABASE[collection].find_one(query)
