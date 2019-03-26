dce3299a226bddab2a9c03cf12718a6110a4e219
Michael Nash <michael@spatialnetworks.com>
2019-03-26 16:49:18 -0400

    added post models

--- TerminalBlog/app.py
+++ TerminalBlog/app.py
@@ -1,11 +1,14 @@
 __author__ = 'utumno86'
 
+from models.post import Post
 import pymongo
 
 uri = "mongodb://127.0.0.1:27017"
 client = pymongo.MongoClient(uri)
-database = client["fullstack"]
-collection = database["posts"]
 
-students = [student for student in collection.find({})]
-print(students)
+post = Post("Post 1 title", "post 1 content", "post 1 author")
+post2 = Post("Post 2 title", "post 2 content", "post 2 author")
+
+print(post.content)
+print(post2.content)
+
---
+++ TerminalBlog/models/post.py
@@ -0,0 +1,8 @@
+__author__ = "utumno86"
+
+class Post(object):
+
+  def __init__(self, title, content, author):
+    self.title = title
+    self.content = content
+    self.author = author
