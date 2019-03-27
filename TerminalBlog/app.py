44ff07ecfa511d1587e74baec4dacade05bb8887
Michael Nash <michael@spatialnetworks.com>
2019-03-27 17:18:27 -0400

    got rid of all the dumb author statements

--- TerminalBlog/app.py
+++ TerminalBlog/app.py
@@ -2,8 +2,6 @@ from models.post import Post
 from database import Database
 import pymongo
 
-__author__ = 'utumno86'
-
 Database.initialize()
 
 posts = Post.from_blog('123')
--- TerminalBlog/database.py
+++ TerminalBlog/database.py
@@ -1,7 +1,5 @@
 import pymongo
 
-__author__ = 'utumno86'
-
 class Database(object):
   URI = "mongodb://127.0.0.1:27017"
   DATABASE = None
