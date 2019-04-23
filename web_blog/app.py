6e49cccaa4dcf8078ab7ecde035abb8aeba3a4af
Michael Nash <michael@spatialnetworks.com>
2019-04-23 16:21:24 -0400

    set up app for logins

---
+++ web_blog/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, session
+
+from src.models.user import User
+from src.common.database import Database
+
+app = Flask(__name__)
+app.secret_key = 'secrets'
+
+@app.route('/')
+def hello_method():
+  return render_template('login.html')
+
+@app.before_first_request
+def initialize_database():
+  Database.initialize()
+
+@app.route('/login', methods=['POST'])
+def login_user():
+  email = request.form['email']
+  password = request.form['password']
+
+  if User.login_valid(email, password):
+    User.login(email)
+
+  return render_template('profile.html', email=session['email'])
+
+if __name__ == '__main__':
+  app.run()
