58fb0155ce59fe0261d305d959e4a36ca47aa6a3
Michael Nash <michael@spatialnetworks.com>
2019-04-25 15:35:31 -0400

    made the stuff actually work

--- web_blog/app.py
+++ web_blog/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, make_response
 
 from src.models.blog import Blog
+from src.models.post import Post
 from src.models.user import User
 from src.common.database import Database
 
@@ -61,7 +62,7 @@ def blog_posts(blog_id):
   blog = Blog.from_mongo(blog_id)
   posts = blog.get_posts()
 
-  return render_template('posts.html', posts=posts, blog_title=blog.title)
+  return render_template('posts.html', posts=posts, blog_title=blog.title, blog_id=blog._id)
 
 @app.route('/blogs/new', methods=['POST', 'GET'])
 def create_new_blog():
@@ -76,6 +77,20 @@ def create_new_blog():
     new_blog.save_to_mongo()
 
     return make_response(user_blogs(user._id))
+
+@app.route('/posts/new/<string:blog_id>', methods=['POST', 'GET'])
+def create_new_post(blog_id):
+  if request.method == 'GET':
+    return render_template('new_post.html', blog_id=blog_id)
+  else:
+    title = request.form['title']
+    content = request.form['content']
+    user = User.get_by_email(session['email'])
+
+    new_post = Post(blog_id, title, content, user.email)
+    new_post.save_to_mongo()
+
+    return make_response(blog_posts(blog_id))
 
 
 if __name__ == '__main__':
