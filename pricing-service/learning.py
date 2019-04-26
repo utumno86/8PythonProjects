cdce99143e225259d0a3dfcfc2fd0a8eb08b230c
Michael Nash <michael@spatialnetworks.com>
2019-04-26 16:23:19 -0400

    some setup

--- pricing-service/learning.py
+++ pricing-service/learning.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 learning_blueprint = Blueprint('learning', __name__)
 
-@learning_blueprint.route('/')
-def home():
-  return "Hello, world"
+@learning_blueprint.route('/<string:name>')
+def home(name):
+  return f"Hello, {name}"
