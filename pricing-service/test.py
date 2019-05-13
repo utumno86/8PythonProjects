ea88bd01440b6a29b385d0bffcd167a42a97b672
Michael Nash <michael@spatialnetworks.com>
2019-05-13 17:32:31 -0400

    core beautiful soup logic

--- pricing-service/test.py
+++ pricing-service/test.py
@@ -1,5 +1,21 @@
+import re
 import requests
+from bs4 import BeautifulSoup
 
-response = requests.get("https://www.johnlewis.com/samsung-addwash-ww90k5410ww-eu-washing-machine-9kg-load-a-energy-rating-1400rpm-spin-white/p2523271")
+URL = "https://www.johnlewis.com/samsung-addwash-ww90k5410ww-eu-washing-machine-9kg-load-a-energy-rating-1400rpm-spin-white/p2523271"
+TAG_NAME = "p"
+QUERY = {"class": "price price--large"}
 
-print(response.content)
+response = requests.get(URL)
+content = response.content
+soup = BeautifulSoup(content, "html.parser")
+element = soup.find(TAG_NAME, QUERY)
+string_price = element.text.strip()
+
+pattern = re.compile(r"(\d+,?\d+\.\d\d)")
+match = pattern.search(string_price)
+found_price = match.group(1)
+without_commas = found_price.replace(",", "")
+price = float(without_commas)
+
+print(price)
