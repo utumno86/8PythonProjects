0051e8b48b4658a0422f67b15df9c081ed778792
Michael Nash <michael@spatialnetworks.com>
2019-05-14 15:22:28 -0400

    started the alert model

---
+++ pricing-service/models/alert.py
@@ -0,0 +1,19 @@
+class Alert:
+  def __init__(self):
+    pass
+
+  def json(self):
+    pass
+
+  def save_to_mongo(self):
+    pass
+
+  @classmethod
+  def all(cls):
+    pass
+
+  def load_item_price(self):
+    pass
+
+  def notify_if_price_reached(self):
+    pass
