fe90363becc6f29c94774768c575ace3fb65e0e8
Michael Nash <michael@spatialnetworks.com>
2019-05-31 17:34:51 -0400

    new item form

--- pricing-service/app.py
+++ pricing-service/app.py
@@ -1,13 +1,19 @@
-# from flask import Flask
-# from learning import learning_blueprint
-from models.alert import Alert
+import json
+from flask import Flask, render_template, request
+from models.item import Item
 
-alert = Alert("2b16b22529e74c92b3d263e5cb29ccd3", 2000)
-alert.save_to_mongo()
+app = Flask(__name__)
 
-# app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def new_item():
+  if request.method == 'POST':
+    url = request.form['url']
+    tag_name = request.form['tag_name']
+    query = json.loads(request.form['query'])
 
-# app.register_blueprint(learning_blueprint)
+    Item(url, tag_name, query).save_to_mongo()
 
-# if __name__ == '__main__':
-#   app.run()
+  return render_template('new_item.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
