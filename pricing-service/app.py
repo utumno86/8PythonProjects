828c71651e3d86dfd9b85d9cdb66f7b7eb329248
Michael Nash <michael@spatialnetworks.com>
2019-06-03 15:24:18 -0400

    view housekeeping

--- pricing-service/app.py
+++ pricing-service/app.py
@@ -1,19 +1,9 @@
-import json
-from flask import Flask, render_template, request
-from models.item import Item
+from flask import Flask
+from views.items import item_blueprint
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def new_item():
-  if request.method == 'POST':
-    url = request.form['url']
-    tag_name = request.form['tag_name']
-    query = json.loads(request.form['query'])
-
-    Item(url, tag_name, query).save_to_mongo()
-
-  return render_template('new_item.html')
+app.register_blueprint(item_blueprint, url_prefix="/items")
 
 if __name__ == "__main__":
     app.run(debug=True)
