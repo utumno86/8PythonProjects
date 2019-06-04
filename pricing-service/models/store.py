78256d2f7d517c63759de76e9e407abba17f19e7
Michael Nash <michael@spatialnetworks.com>
2019-06-04 18:07:08 -0400

    store file work in progress

---
+++ pricing-service/models/store.py
@@ -0,0 +1,37 @@
+import uuid
+import re
+from typing import Dict
+from models.model import Model
+
+class Store(Model):
+    collection = "stores"
+
+    def __init__(self, url_prefix: str, tag_name: str, query: Dict, _id: str = None):
+      super().__init__()
+      self.url_prefix = url_prefix
+      self.tag_name = tag_name
+      self.query = query
+      self.price = None
+      self._id = _id or uuid.uuid4().hex
+
+    def json(self) -> Dict:
+      return {
+        "_id": self._id,
+        "url": self.url,
+        "tag_name": self.tag_name,
+        "query": self.query
+      }
+
+    @classmethod
+    def get_by_name(cls, store_name: str) -> "Store":
+      return cls.find_one_by("name", store_name)
+
+    @classmethod
+    def get_by_url_prefix(cls, url_prefix: str) -> "Store":
+      url_regex = {"$regex": "^{}".format(url_prefix)}
+      return cls.find_one_by("url_prefix", url_regex)
+
+    @classmethod
+    def find_by_url(cls, url: str) -> "Store":
+      pattern = re.compile(r"(https?:\/\/.*\/)/g")
+      match = pattern.search(url)
